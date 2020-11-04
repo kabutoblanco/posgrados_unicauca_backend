9a083d05e829c806473890fd3a0a55bf8281c0d9
danielgaviri4 <gdaniel@unicauca.edu.co>
2020-11-04 10:47:06 -0500

    Subo modificacion de algunas apis anterioprmente subidas.

--- a_students_app/urls.py
+++ a_students_app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet, UserViewSet, EnrrollmentViewSet, StudentApiView
+from .views import StudentViewSet, UserViewSet, EnrrollmentViewSet,ProgramViewSet
 
 router = DefaultRouter()
 router.register('student', StudentViewSet)
 router.register('user', UserViewSet)
 router.register('enrrollment', EnrrollmentViewSet)
+router.register('program', ProgramViewSet)
 
 
 
 
 urlpatterns = [
-    path('',include(router.urls)),
-    path('api/student/all',StudentApiView.as_view())
-]
+    path('',include(router.urls))
+    ]
