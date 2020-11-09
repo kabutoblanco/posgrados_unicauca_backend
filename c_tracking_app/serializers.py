843479429dab8d391c8994fb683f75812453d747
mdquilindo <mdquilindo@unicauca.edu.co>
2020-11-08 20:33:59 -0500

    get type value activity

--- c_tracking_app/serializers.py
+++ c_tracking_app/serializers.py
@@ -14,8 +14,7 @@ class TypeActiviyField(serializers.Field):
         super(TypeActiviyField, self).__init__(**kwargs)
 
     def to_representation(self, obj):
-        # return obj
-        return self._choices.get(obj)
+        return self._choices[0]
 
     def to_internal_value(self, data):
         return getattr(self._choices, data)
