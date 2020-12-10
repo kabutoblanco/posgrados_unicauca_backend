0f29365f0cb7618d8e2f817995629ccaf33c689f
Jeison Ortiz <jesonog@gmail.com>
2020-12-10 09:02:20 -0500

    se corrigio lo que se deberia haber subido en el commit anterior x2

--- d_information_management_app/models.py
+++ d_information_management_app/models.py
@@ -415,29 +415,4 @@ class CoordinatorProgram(models.Model):
         verbose_name = "Coordinador"
         verbose_name_plural = "Coordinadores"
 
-class CoordinatorProgram(models.Model):
-    """
-    Clase usada para tener el registro de la relacion entre un profesor y un programa de la universidad en el cual
-    puede o no ser el coordinador del programa
-    - - - - -
-    Attributes
-    - - - - -
-    professor : int
-        Referencia a un profesor
-    program : int
-        Referencia a un programa de la universidad
-    academic_period : string[10]
-        Periodo en el cual el profesor es coordinador del programa
-    """
-    professor = models.ForeignKey(Professor, on_delete=models.SET_NULL, blank=False, null=True)
-    program = models.ForeignKey('a_students_app.Program', on_delete=models.SET_NULL, blank=False, null=True)
-    academic_period = models.CharField(max_length=10)
-    date_record = models.DateTimeField(auto_now=True)
-    date_update = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
-
-    class Meta:
-        unique_together = ("professor", "academic_period")
-        verbose_name = "Coordinador"
-        verbose_name_plural = "Coordinadores"
 
