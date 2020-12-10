9561795430e9293b4f796e95af73314b57539f69
Jeison Ortiz <jesonog@gmail.com>
2020-12-09 23:03:38 -0500

    se agrego el modelo para el control de los coordinadores de cada programa

--- d_accounts_app/models.py
+++ d_accounts_app/models.py
@@ -17,10 +17,10 @@ class User(AbstractUser):
     type_id = models.IntegerField(verbose_name='Tipo de ID', choices=ID_CHOICES, default=1)
     personal_id = models.CharField(verbose_name='Numero de ID', max_length=24)
     personal_code = models.CharField(verbose_name='Codigo ID', max_length=24)
-    photo = models.FileField(verbose_name='Foto', upload_to="d_accounts_app/users/%Y/%m/%d")
+    photo = models.FileField(verbose_name='Foto', upload_to="d_accounts_app/users/%Y/%m/%d", default="/default/default_user.png")
     telephone = models.CharField(verbose_name='Telefono', max_length=24)
     address = models.CharField(verbose_name='Direccion', max_length=64)
-
+    is_coordinator = models.BooleanField(verbose_name="Es Coordinador", default=False)
     is_proffessor = models.BooleanField(verbose_name='Es profesor', default=False)
     is_student = models.BooleanField(verbose_name='Es estudiante', default=True)
 
