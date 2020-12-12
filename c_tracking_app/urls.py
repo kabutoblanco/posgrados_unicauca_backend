df225fff0ea359fed827edff8f4439385bf3f464
mdquilindo <mdquilindo@unicauca.edu.co>
2020-12-11 21:12:56 -0500

    envio de correo a coordinador y estudiantes cuando se calificac una actividad

--- c_tracking_app/urls.py
+++ c_tracking_app/urls.py
@@ -13,14 +13,14 @@ router.register('test_coordinator', TestCoordinatorAPI)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/student', StudentListAPI.as_view()),
+    path('api/student', StudentListAPI.as_view(), name='list-student'),
     path('api/student/<int:id_student>', StudentAPI.as_view()),
-    path('api/student/<int:id_student>/activity', ActivityListStudentAPI.as_view()),
+    path('api/student/<int:id_student>/activity', ActivityListStudentAPI.as_view(), name='list-student-activity'),
 
     path('api/director/<int:id_professor>/activity', DirectorActiviesAPI.as_view()),
     path('api/director/<int:id_professor>/student', DirectorStudentsAPI.as_view()),
-    path('api/coordinator/<int:id_professor>/activity', ActivityCoordinatorAPI.as_view()),
+    path('api/coordinator/<int:id_professor>/activity', CoordinatorActivitiesAPI.as_view()),
 
-    path('api/director/<int:id_professor>/<int:id_activity>/test', TestDirectorListAPI.as_view()),
-    path('api/coordinator/<int:id_professor>/<int:id_activity>/test', TestCoordinatorListAPI.as_view()),
+    path('api/director/<int:id_professor>/test', TestDirectorListAPI.as_view()),
+    path('api/coordinator/<int:id_professor>/test', TestCoordinatorListAPI.as_view()),
 ]
